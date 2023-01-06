133a82cbcad6f5b84d00877e979085624ad78223
Davar96 <david.arustam96@gmail.com>
2023-01-06 20:16:27 +0100

    done the footer and the back to top btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,7 +131,6 @@ const setLetterEffect = function () {
 window.addEventListener("load", setLetterEffect);
 
 //SCROLL REVEAL
-
 const revealElements = document.querySelectorAll("[data-reveal]");
 
 const scrollReveal = function () {
@@ -150,3 +149,22 @@ const scrollReveal = function () {
 window.addEventListener("scroll", scrollReveal);
 
 scrollReveal();
+
+//  BACK TO TOP BUTTON
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  const bodyHeight = document.body.scrollHeight;
+  const windowHeight = window.innerHeight;
+  const scrollEndPos = bodyHeight - windowHeight;
+  const totalScrollPercent = (window.scrollY / scrollEndPos) * 100;
+
+  backTopBtn.textContent = `${totalScrollPercent.toFixed(0)}%`;
+
+  // visible back top btn when scrolled 5% of the page
+  if (totalScrollPercent > 5) {
+    backTopBtn.classList.add("show");
+  } else {
+    backTopBtn.classList.remove("show");
+  }
+});
